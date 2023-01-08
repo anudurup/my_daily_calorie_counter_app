e89f3ef5b2a7a1cbe72e6ec7cdf307a63fddcdc7
anudurup <alekhya.sai.nuduru.pati@intel.com>
2023-01-07 16:50:46 -0800

    Update meal per day and daily trackers

---
+++ replicate.py
@@ -0,0 +1,14 @@
+mealtype = ['breakfast','smoothie','lunch','snack','salad','dinner']
+out_f = open('replicated.txt','a')
+for meal in mealtype:
+    f = open('replicate.txt')
+    lines = f.readlines()
+    for i,line in enumerate(lines):
+        if 'Breakfast' in line:
+            lines[i] = lines[i].replace('Breakfast',meal.title())
+        elif 'breakfast' in line:
+            lines[i] = lines[i].replace('breakfast',meal)
+    out_f.writelines(lines)
+    out_f.write("\n\n")
+    f.close()
+out_f.close()
